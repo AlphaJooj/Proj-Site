4c11cebfdd7da66f85f33c62a3ee33c9cbc2d62a
Vinicius Lima <vinixd476@gmail.com>
2018-11-06 16:25:13 -0200

    Upando o Projeto

---
+++ javascript.js
@@ -0,0 +1,18 @@
+
+
+
+/*document.querySelector("#menu").onclick = alertar;*/
+
+
+
+function Iniciar() {
+
+	document.querySelector(".rodape").onclick = alertar;
+	
+}
+
+function alertar(){
+	alert("Teste");
+}
+
+window.onload = Iniciar;
